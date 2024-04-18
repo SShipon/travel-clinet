f93622a45bb16822072bf087dff3b77778324675
sabrina <sabrinaahmednitu@gmail.com>
2024-04-19 03:19:50 +0600

    contact icon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -16,22 +16,22 @@ const Contact = () => {
             <div className="flex gap-3">
               <div className="search-container">
                 <div className="search-box">
-                  <i class="fa-solid fa-magnifying-glass "></i>
+                  <i class="fa-brands fa-facebook"></i>
                 </div>
               </div>
               <div className="search-container">
                 <div className="search-box">
-                  <i class="fa-solid fa-magnifying-glass "></i>
+                  <i class="fa-brands fa-twitter"></i>
                 </div>
               </div>
               <div className="search-container">
                 <div className="search-box">
-                  <i class="fa-solid fa-magnifying-glass "></i>
+                  <i class="fa-brands fa-instagram"></i>
                 </div>
               </div>
               <div className="search-container">
                 <div className="search-box">
-                  <i class="fa-solid fa-magnifying-glass "></i>
+                  <i class="fab fa-dribbble"></i>
                 </div>
               </div>
             </div>
@@ -55,21 +55,6 @@ const Contact = () => {
           </form>
         </section>
         <section className="location-section">
-          <div className="location-text">
-            <br /> <br />
-            <br />
-            <br />
-            <p>
-              Newmarket is a town and regional seat of the Regional Municipality
-              of York in the Canadian province of Ontario. It is part of Greater
-              Toronto in the Golden Horseshoe region of Southern Ontario. The
-              name stems from the fact that the settlement was a "New Market",
-              in contrast to York as the Old Market.
-            </p>
-            <br />
-            <h1>Province: Ontario</h1>
-            <h1>Regional municipality: York Region</h1>
-          </div>
           <div className="location-map">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235373.9083414654!2d89.27794933319095!3d22.812936138465016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff91780094f875%3A0x17bf6cf5c0e7b50b!2sDak%20bangla%20more%20Boro%20Bazar%20khulna%20Bangladesh!5e0!3m2!1sen!2sbd!4v1695062048906!5m2!1sen!2sbd"
