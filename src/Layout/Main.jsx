fc17fef6666df056824f649c7fc34d37b63a01e8
SShipon <nxshipon@gmail.com>
2024-04-18 23:09:03 +0600

    navbar add

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia omnis quos earum pariatur sed numquam consectetur aut adipisci sit debitis.</h3>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/components/Home/CountryPlace/CountryPlace.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountryPlace = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CountryPlace;
