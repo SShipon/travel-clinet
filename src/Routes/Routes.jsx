67699d3b39565a7baf0040a070329f0475c9a6c6
sabrina <sabrinaahmednitu@gmail.com>
2024-04-21 12:51:05 +0600

    nav link

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Contact from "../Pages/Contact/Contact";
+import Home from "../Pages/Home/Home";
+import Page from "../Pages/Page/Page";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main></Main>,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '/page',
+    element: <Page />,
   },
   {
     path: '/contact',
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -10,42 +10,54 @@ const Navbar = () => {
 
 
     return (
-        <header className="bg-white py-10">
-            <nav className="flex justify-between items-center w-[92%] mx-auto">
-                <div className=''>
-                  <img src={travel} alt="" srcset="" />
-                </div>
-                <div className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${isMenuOpen ? 'top-[9%]' : 'top-[-100%]'} md:w-auto w-full flex items-center px-5`}>
-                    <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 ">
-                        <li>
-                            <a to='/home' className="hover:text-gray-500" >Home</a>
-                        </li>
-                        <li>
-                            <a className="hover:text-gray-500" >Destinations</a>
-                        </li>
-                        <li>
-                            <a className="hover:text-gray-500" >Tours</a>
-                        </li>
-                        <li>
-                            <a className="hover:text-gray-500" >Pages</a>
-                        </li>
-                        <li>
-                            <a className="hover:text-gray-500" >News</a>
-                        </li>
-                        <Link to="/contact">
-                            <a className="hover:text-gray-500"> Contact</a>
-                        </Link>
-                    </ul>
-                </div>
-                <div className="flex items-center gap-6">
-                    <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]" onClick={toggleMenu}>Sign in</button>
-                 
-                   <ion-icon name={isMenuOpen ? "close" : "menu"} onClick={toggleMenu} className="text-3xl cursor-pointer  
-                   "></ion-icon>
-                  
-                </div>
-            </nav>
-        </header>
+      <header className="bg-white py-10">
+        <nav className="flex justify-between items-center w-[92%] mx-auto">
+          <div className="">
+            <img src={travel} alt="" srcset="" />
+          </div>
+          <div
+            className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${
+              isMenuOpen ? 'top-[9%]' : 'top-[-100%]'
+            } md:w-auto w-full flex items-center px-5`}
+          >
+            <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 ">
+              <Link to="/home">
+                <a className="hover:text-gray-500">Home</a>
+              </Link>
+              <li>
+                <a className="hover:text-gray-500">Destinations</a>
+              </li>
+              <li>
+                <a className="hover:text-gray-500">Tours</a>
+              </li>
+              <Link to="/page">
+                <a className="hover:text-gray-500">Pages</a>
+              </Link>
+              <li>
+                <a className="hover:text-gray-500">News</a>
+              </li>
+              <Link to="/contact">
+                <a className="hover:text-gray-500"> Contact</a>
+              </Link>
+            </ul>
+          </div>
+          <div className="flex items-center gap-6">
+            <button
+              className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]"
+              onClick={toggleMenu}
+            >
+              Sign in
+            </button>
+
+            <ion-icon
+              name={isMenuOpen ? 'close' : 'menu'}
+              onClick={toggleMenu}
+              className="text-3xl cursor-pointer  
+                   "
+            ></ion-icon>
+          </div>
+        </nav>
+      </header>
     );
 };
 
